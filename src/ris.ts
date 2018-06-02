016267cc66b7c7f0ac7aaaa224199a1fe845424b
drkchiloll <samwomack_@me.com>
2018-06-02 09:53:30 -0500

    prepare for better abstraction

--- src/ris.ts
+++ src/ris.ts
@@ -6,25 +6,27 @@ export const risdoc = `
   >
     <soapenv:Header/>
     <soapenv:Body>
-       <soap:selectCmDevice xmlns:ns1="http://schemas.cisco.com/ast/soap/"
-                            soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
-                            xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">
-          <soap:StateInfo/>
-          <soap:CmSelectionCriteria>
-             <soap:MaxReturnedDevices>1000</soap:MaxReturnedDevices>
-             <soap:Class>Phone</soap:Class>
-             <soap:DeviceClass>Phone</soap:DeviceClass>
-             <soap:Model>255</soap:Model>
-             <soap:Status>Registered</soap:Status>
-             <soap:NodeName xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>
-             <soap:SelectBy></soap:SelectBy>
-             <soap:SelectItems>
-                <!--Zero or more repetitions:-->
-             </soap:SelectItems>
-             <soap:Protocol>Any</soap:Protocol>
-             <soap:DownloadStatus>Any</soap:DownloadStatus>
-          </soap:CmSelectionCriteria>
-       </soap:selectCmDevice>
+      <soap:selectCmDevice
+        xmlns:ns1="http://schemas.cisco.com/ast/soap/"
+        soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
+        xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
+      >
+        <soap:StateInfo/>
+        <soap:CmSelectionCriteria>
+          <soap:MaxReturnedDevices>1000</soap:MaxReturnedDevices>
+          <soap:Class>ANY</soap:Class>
+          <soap:DeviceClass>ANY</soap:DeviceClass>
+          <soap:Model>255</soap:Model>
+          <soap:Status>Registered</soap:Status>
+          <soap:NodeName xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>
+          <soap:SelectBy></soap:SelectBy>
+          <soap:SelectItems>
+            <!--Zero or more repetitions:-->
+          </soap:SelectItems>
+          <soap:Protocol>Any</soap:Protocol>
+          <soap:DownloadStatus>Any</soap:DownloadStatus>
+        </soap:CmSelectionCriteria>
+      </soap:selectCmDevice>
     </soapenv:Body>
  </soapenv:Envelope>
 `
